77cade28860b8d5823c0ce54032aa37828e6cc00
taqihaider591 <taqihaider591@gmail.com>
2024-08-12 03:36:32 +0500

    P1

---
+++ test.js
@@ -0,0 +1,14 @@
+console.log("Taqi Haider");
+
+console.log("Welcome to NED");
+
+// age = 24
+let FullName = "Taqi"
+let x = null
+let undefined
+console.log(FullName);
+console.log(x);
+console.log(undefined);
+
+const age = 154.55;
+console.log(age);
