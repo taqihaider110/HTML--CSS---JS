0db6d649f24301cfd48a6566cafb35f483601b84
taqihaider110 <taqihaider591@gmail.com>
2025-11-27 15:50:51 +0500

    Props

---
+++ React/book-listing - Props/src/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import img2 from "./images/Goku.jpeg";
+
+const firstBook = {
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmhGeDd0pCpEpBTWxjcXymXZY8uGUX86GQWg&s",
+  title: "One Man Army",
+  author: "Hekiuro Tsuruta",
+};
+
+const secondBook = {
+  img: img2,
+  title: " Dragon ball",
+  author: " Akira Toriyama",
+};
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+    </section>
+  );
+};
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      {/* <Image />
+      <Author />
+      <Title /> */}
+      <h1>{props.title}</h1>
+      <h3>{props.author}</h3>
+      <img src={props.img} alt={props.title} />
+    </article>
+  );
+};
+
+// const Image = () => {
+//   return (
+//     <img
+//       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmhGeDd0pCpEpBTWxjcXymXZY8uGUX86GQWg&s"
+//       alt="book cover"
+//     />
+//   );
+// };
+// const Author = () => {
+//   return <h1> Taqi Haider</h1>;
+// };
+
+// const Title = () => {
+//   return <h1> Coffee and books</h1>;
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
